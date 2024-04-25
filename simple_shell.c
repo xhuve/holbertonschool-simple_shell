2162ccd2517d023da526d3475fc590962afe1133
xhuve <xhuvelialbana@gmail.com>
2024-04-25 14:50:31 +0200

    Fix pipe command

--- simple_shell.c
+++ simple_shell.c
@@ -4,17 +4,27 @@ int main(void)
 {
 	while(1)
 	{
+		int istty;
 		char *exec_args[128];
 		
-		if(command_read(exec_args) != 0)
+		istty = isatty(STDIN_FILENO);
+
+		if (istty == 1)
+			printf("$ ");
+
+		if (command_read(exec_args) != 0)
 			exit(EXIT_FAILURE);
 		
-		if (exec_args[0] == NULL || strspn(exec_args[0], " ") == strlen(exec_args[0]))
+		if (*exec_args[0] == '\n')
 			continue;
 
 		if(execute(exec_args) != 0)
 			exit(EXIT_FAILURE);
 
+		if (istty != 1)
+			break;
+
+		free(*exec_args);
 	}
 	return (0);
 }
